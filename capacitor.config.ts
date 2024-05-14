40f350f089ce6224725ab4ce86152d8f75c96f41
Tiestof <tiestof@gmail.com>
2024-05-14 13:09:10 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.examenhibrida',
+  appName: 'examenhibrida',
+  webDir: 'www'
+};
+
+export default config;
