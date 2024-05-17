d9a72135daba22faddf50cac77c77791e36c69e2
Tiestof <tiestof@gmail.com>
2024-05-16 23:30:29 -0400

    Examen Final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Publicacion } from '../Modelo/ListaPublicaciones';
 import { publicacionService } from '../Servicios/persistencia-publicaciones.service';
-import { ListaPublicacionesComponent } from '../Componentes/lista-publicaciones/lista-publicaciones.component';
+import { ListaPublicacionesComponent } 
+         from '../Componentes/lista-publicaciones/lista-publicaciones.component';
 import { RouterModule } from '@angular/router';
-import { IonFabButton, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonFabButton, IonIcon, IonHeader, IonToolbar, 
+          IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [RouterModule, IonFabButton, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, ListaPublicacionesComponent],
+  imports: [RouterModule, IonFabButton, IonIcon, IonHeader, 
+            IonToolbar, IonTitle, IonContent, ListaPublicacionesComponent],
 })
 export class HomePage implements OnInit {
 
